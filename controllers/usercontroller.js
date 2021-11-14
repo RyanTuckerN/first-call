bd84a8bfe97bbbfea33fe7cd6b890700b704f385
Ryan Tucker <nick.tucker.bass@gmail.com>
2021-11-13 19:43:26 -0500

    .

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -231,7 +231,7 @@ router.get("/profile/:id", validateSession, async (req, res) => {
       where: { id },
       include: {
         model: Story,
-        order: 'ASC',
+        order: [[id, 'DESC']],
         include: [
           {
             model: Post,
