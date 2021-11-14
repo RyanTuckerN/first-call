361d2db5c76c495048b1fc960fc86c86bde0f3ed
Ryan Tucker <nick.tucker.bass@gmail.com>
2021-11-13 19:39:42 -0500

    asc stories to profile

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -231,7 +231,7 @@ router.get("/profile/:id", validateSession, async (req, res) => {
       where: { id },
       include: {
         model: Story,
-        order: 'DESC',
+        order: 'ASC',
         include: [
           {
             model: Post,
